7896f77847d109f0c3904934e47f2e6a368add68
hanshuo <hanshuo0317@qq.com>
2019-09-05 16:17:03 +0800

    第一次修改

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -71,5 +71,8 @@ class IndexController extends Controller
     {
         return view('/index/xufei');
     }
-
+     public function xufeisss()
+    {
+        return view('/index/xufeisss');
+    }
 }
